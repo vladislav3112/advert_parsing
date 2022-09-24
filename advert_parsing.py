74f5b8dfe2450c1f5cdfa4aaa734a65d2cb6badf
dyushakolesov <dyushakolesov@mail.ru>
2022-09-24 21:14:55 +0300

    task done

--- advert_parsing.py
+++ advert_parsing.py
@@ -1,16 +1,42 @@
 import json
 
+"""Класс Advert для парсинга объявлений в формате JSON:
+    1. динамичесĸи создает атрибуты эĸземпляра ĸласса из атрибутов JSON-объеĸта
+    2. имеет свойство price и 
+        ○ проверяет, что устанавливаемое значение не отрицательно
+        ○ в случае отсутствия поля price в JSON-объеĸте возвращает 0
+    3. метод __repr__, ĸоторый выводит название и цену объявления
+    
+    Класс ColorizeMixin:
+        ○ меняет цвет теĸста при выводе на ĸонсоль
+        ○ задает цвет в атрибуте ĸласса repr_color_code"""
+
 
 def json_to_dict(json_filename):
     with open(json_filename, encoding="utf-8") as json_file:
         return json.load(json_file)
 
+
 class ColorizeMixin:
-    repr_color_code = 32 # green
+    # список цветов
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"  # вернуть цвет
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    # текущий цвет
+    repr_color_code = WARNING
+
 
 class Advert(ColorizeMixin):
     def __repr__(self):
-        return f'{self.title} | {self.price} ₽'
+        return f"{ColorizeMixin.repr_color_code} {self.title} | {self.price} ₽ {ColorizeMixin.ENDC}"
+
     def __init__(self, mapping):
         for atribute in mapping:
             curr_atribute = atribute
@@ -19,19 +45,27 @@ class Advert(ColorizeMixin):
                 setattr(self, curr_atribute, Advert(curr_dict))
             else:
                 setattr(self, curr_atribute, mapping[curr_atribute])
-        if hasattr(self, 'price'):
+        if hasattr(self, "price"):
             if self.price < 0:
-                raise ValueError('Price must be >= 0')
+                raise ValueError("Price must be >= 0")
         else:
             self.price = 0
 
+
 if __name__ == "__main__":
+    # sample test
     dictionary = json_to_dict("sample_ad.json")
     lesson_ad = Advert(dictionary)
-    assert lesson_ad.location.address == 'город Москва, Лесная, 7'
+    assert lesson_ad.location.address == "город Москва, Лесная, 7"
     assert lesson_ad.price == 0
     print(lesson_ad)
-    
-    dictionary2 = json_to_dict("price_test_ad.json")
-    lesson_ad2 = Advert(dictionary2)
-    
+
+    # проверка смены цвета вывода в консоль
+    dictionary_corgi = json_to_dict("corgi.json")
+    corgi_ad = Advert(dictionary_corgi)
+    print(corgi_ad)
+    corgi_ad.__class__
+
+    # price < 0
+    dictionary_fail = json_to_dict("price_test_ad.json")
+    fail_ad = Advert(dictionary_fail)
